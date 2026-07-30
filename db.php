0c5cb159e581f721d6d803774a5d5c8d92e5db6e
Ivan Clyd Suello <suelloclyd7@gmail.com>
2026-07-30 12:02:12 +0800

    Update database connection settings

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
 
-    $conn = mysqli_connect(
-        "localhost",
-        "root",
-        "",
-        "ticket_system"
-    );
+$conn = mysqli_connect(
+    "sql308.infinityfree.com",
+    "if0_42361013",
+    "jiIx11RO91oVeo",
+    "if0_42361013_movie_ticket"
+);
 
-    if(!$conn){
-        die("Connection failed" . mysqli_connect_error());
-    }
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 ?>
